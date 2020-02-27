48f1b2328fe75466aaf6d5856e4e32bc79f2ab7e
leooangelo <leozinhosp321@hotmail.com>
2020-02-27 14:17:39 -0300

    ToString de pedido

--- src/main/java/br/com/leonardoangelo/app/service/PedidoService.java
+++ src/main/java/br/com/leonardoangelo/app/service/PedidoService.java
@@ -3,6 +3,8 @@ package br.com.leonardoangelo.app.service;
 import java.util.Date;
 import java.util.Optional;
 
+import javax.print.attribute.standard.PDLOverrideSupported;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,6 +37,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 	
 
 	public Pedido find(Integer id) {
@@ -47,6 +52,7 @@ public class PedidoService {
 	public Pedido insert( Pedido pedido) {
 		pedido.setId(null);
 		pedido.setInstante(new Date());
+		pedido.setCliente(clienteService.find(pedido.getCliente().getId()));
 		pedido.getPagamento().setEstado(EstadoPagamento.PENDENETE);
 		pedido.getPagamento().setPedido(pedido);
 		
@@ -59,11 +65,12 @@ public class PedidoService {
 		
 		for(ItemPedido ip : pedido.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(pedido);
 		}
 		itemPedidoRepository.saveAll(pedido.getItens());
-		
+		System.out.println(pedido);
 		return pedido;
 	}
 
